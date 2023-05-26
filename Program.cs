0d1d0f25e3df162d679c86495bf6fcf6cd1e4903
birendraakumar <birendraa.kumar@gmail.com>
2023-05-26 11:22:58 +0530

    LFS Commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using Spire.Doc;
+
+namespace ReadWordFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Document doc = new Document("C:\\Users\\HP\\Desktop\\document.docx");
+            //doc.Range.Replace("sad", "[replaced]", new FindReplaceOptions(FindReplaceDirection.Forward));
+            ////doc.Save("C:\\Users\\HP\\Desktop\\Find-And-Replace-Text.docx");
+            //doc.Save("C:\\Users\\HP\\Desktop\\Find-And-Replace-Text.pdf", SaveFormat.Pdf);
+
+            //Document doc = new Document();
+            //doc.LoadFromFile("C:\\Users\\HP\\Desktop\\document.docx");
+            //doc.Replace("passionate", "[replaced]", true, true);
+            //doc.SaveToFile("C:\\Users\\HP\\Desktop\\FindandReplace.pdf", FileFormat.PDF);
+
+            Document doc = new Document();
+            doc.LoadFromFile("C:\\Users\\HP\\Desktop\\Back_Format.docx");
+            doc.Replace("{{@DOCTORNAME}}", "LOUIS C JORDAN M.D.", true, true);
+            doc.Replace("{{@NPI}}", "1316976632", true, true);
+            doc.Replace("{{@ADDRESS}}", "5716 Cleveland St Suite 200 Virginia Beach VA 23462", true, true);
+            doc.Replace("{{@PHONENUMBER}}", "(757) 502-8570", true, true);
+
+            doc.Replace("{{@PATIENT_NAME}}", "CAROLYN HEDRICK", true, true);
+            doc.Replace("{{@ADDRESS1}}", "1765 WHITE RIDGE RD", true, true);
+            doc.Replace("{{@ADDRESS2}}", "SUTHERLIN VA 24594", true, true);
+            doc.Replace("{{@PATIENTPHONE}}", "(434) 822-5034", true, true);
+            doc.Replace("{{@PATIENTHEIGHT}}", "5’5", true, true);
+            doc.Replace("{{@PATIENTWEIGHT}}", "128", true, true);
+            doc.Replace("{{@PATIENTDOB}}", "October 20 1940", true, true);
+            doc.Replace("{{@PATENTGENDER}}", "Female", true, true);
+
+            doc.Replace("{{@MEDICARE}}", "9F71F47AE99", true, true);
+            doc.Replace("{{@WAISTSIZE}}", "M", true, true);
+            doc.Replace("{{@TREATMENTS}}", "REST", true, true);
+            doc.Replace("{{@LEVELOFPAIN}}", "8 (Severe)", true, true);
+            doc.Replace("{{@EXPERIENCINGTHEPAIN}}", "5 YEARS", true, true);
+
+            doc.Replace("{{@DATE}}", "05-12-2023", true, true);
+            doc.Replace("{{@FULLTIMESTAMP}}", "05-12-2023 01:11:22 PM EST IP 98.183.152.34 ]", true, true);
+
+            doc.SaveToFile("C:\\Users\\HP\\Desktop\\FindandReplace.pdf", FileFormat.PDF);
+            /**************************************************************/
+        }
+
+
+    }
+}
---
+++ ReadMSWF/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadMSWF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //print();
+        }
+
+        static private void FindAndReplace(Microsoft.Office.Interop.Word.Application WordApp, object findText, object replaceWithText)
+        {
+            object matchCase = true;
+            object matchWholeWord = true;
+            object matchWildCards = false;
+            object matchSoundsLike = false;
+            object nmatchAllWordForms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiacritics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object read_only = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = Microsoft.Office.Interop.Word.WdFindWrap.wdFindContinue;
+            object replaceAll = Microsoft.Office.Interop.Word.WdReplace.wdReplaceAll;
+            WordApp.Selection.Find.Execute(ref findText, ref matchCase, ref matchWholeWord, ref matchWildCards, ref matchSoundsLike,
+            ref nmatchAllWordForms, ref forward,
+            ref wrap, ref format, ref replaceWithText,
+            ref replaceAll, ref matchKashida,
+            ref matchDiacritics, ref matchAlefHamza,
+            ref matchControl);
+        }
+        static public void print()
+        {
+            Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
+            //string clearancename = Form1.Texts;
+            Microsoft.Office.Interop.Word.Document doc = app.Documents.Open("C:\\Users\\HP\\Desktop\\document.docx");
+            Microsoft.Office.Interop.Word.Words wds = doc.Sections[1].Range.Words;
+            doc.Activate();
+
+            //Iterate the word need to change font
+            foreach (Microsoft.Office.Interop.Word.Range wd in wds)
+            {
+                if (wd.Text.Equals("<") || wd.Text.Equals(">") || wd.Text.Equals("name"))
+                    wd.Font.Color = Microsoft.Office.Interop.Word.WdColor.wdColorBlack;
+            }
+
+            FindAndReplace(app, "Sad", "[Birendra Kumar]");
+
+            //doc.PrintPreview();
+            object FileFormat = Microsoft.Office.Interop.Word.WdSaveFormat.wdFormatPDF;
+            doc.SaveAs2("C:\\Users\\HP\\Desktop\\Find-And-Replace-Text.pdf", ref FileFormat);
+        }
+    }
+}
